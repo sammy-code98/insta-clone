2c78dc497a7b95a3e37759184cfe77ac4baa9513
sammy-code98 <sammysmart98@yahoo.com>
2021-11-20 14:33:15 +0100

    feat: connect seed file to firestore - connect to db - add config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,10 +1,17 @@
 import Firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
+// import seed file
+// import {seedDatabase} from '../seed'
 
-// call the seed file here
-
-const config = {};
+const config = {
+    apiKey: "AIzaSyA-HP71mBTl9uxu3cMjm1m3Vt4nI9yjx3k",
+    authDomain: "insta-clone-706ca.firebaseapp.com",
+    projectId: "insta-clone-706ca",
+    storageBucket: "insta-clone-706ca.appspot.com",
+    messagingSenderId: "288139497243",
+    appId: "1:288139497243:web:e58fd3939df37197a8cdbe"
+};
 
 // initalize firebase
 
@@ -12,6 +19,9 @@ const firebase = Firebase.initializeApp(config);
 
 const { FieldValue } = Firebase.firestore;
 
+// call seed file once here
 // seedDatabase(firebase)
+
+console.log('firebase :', firebase);
 
 export { firebase, FieldValue}
