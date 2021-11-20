13c61ee8a5308bb9a4b6e83f6761b6ad4a0e0cbb
sammy-code98 <sammysmart98@yahoo.com>
2021-11-20 22:25:05 +0100

    feat: add routed to routes.js and refacorr

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import * as ROUTES from "./constants/routes"
 const Login = lazy(() => import("./pages/login"));
 function App() {
   return (
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path={ROUTES.LOGIN} element={<Login />} />
         </Routes>
       </Suspense>
     </Router>
