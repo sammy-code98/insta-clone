37349db47310dcff45396a5a270c4de26b8963bc
sammy-code98 <sammysmart98@yahoo.com>
2021-11-20 22:11:19 +0100

    changed component to element on route param

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+const Login = lazy(() => import("./pages/login"));
 function App() {
   return (
-    <div>
-     hello
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function login() {
+    return (
+        <div>
+            logoogo
+        </div>
+    )
+}
+
+export default login
