407be8da7d9e5c8f8d72fd87cf83b5ca036b19e3
sammy-code98 <sammysmart98@yahoo.com>
2022-01-01 21:37:47 +0100

    feat: add routes and sign up input fields

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -41,6 +41,20 @@ function SignUp() {
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
           <form onSubmit={handleSignUp} method="POST">
+          <input
+              aria-label="Enter your Username"
+              type="text"
+              placeholder="Username"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+             <input
+              aria-label="Enter your Fullname"
+              type="text"
+              placeholder="FullName"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setFullName(target.value)}
+            />
             <input
               aria-label="Enter your email address"
               type="text"
@@ -62,15 +76,15 @@ function SignUp() {
                 isInvalid && "opacity-50"
               }`}
             >
-              Log In
+              Sign Up
             </button>
           </form>
         </div>
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
           <p className="text-sm">
-            Don't have an account? {``}
-            <Link to="/signup" className="font-bold text-blue-medium">
-              Sign Up
+             Have an account? {``}
+            <Link to="/login" className="font-bold text-blue-medium">
+              Log In
             </Link>
           </p>
         </div>
