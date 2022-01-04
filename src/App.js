4ed31ed2fbd9ece5a1d214e0f0a23705a055e0b7
sammy-code98 <sammysmart98@yahoo.com>
2022-01-04 15:36:59 +0100

    fix: add dashboard and its route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 const Login = lazy(() => import("./pages/Login"));
 const Signup = lazy(() => import("./pages/Signup"));
+const Dashboard = lazy(() => import("./pages/Dashboard"))
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+          <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
           <Route path="*" element={<NotFound />} />
 
         </Routes>
---
+++ src/components/Timeline.js
@@ -0,0 +1,9 @@
+function Timeline() {
+  return (
+    <div>
+      <p>i m the timeline</p>
+    </div>
+  );
+}
+
+export default Timeline;
