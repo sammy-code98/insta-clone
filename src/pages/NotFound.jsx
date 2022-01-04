b703caa7706e6e853ecd38aaaa7259f56081575e
sammy-code98 <sammysmart98@yahoo.com>
2022-01-04 15:03:54 +0100

    feat: error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,15 @@
-
+import { useEffect } from "react";
 function NotFound() {
-    return (
-        <div className="bg-gray-background">
-            <div className="mx-auto max-w-screen-lg">
-                <p className="text-center text-2xl">Not Found!</p>
-            </div>
-        </div>
-    )
+    useEffect(() => {
+        document.title = "404 || Not Found";
+    },[])
+  return (
+    <div className="bg-gray-background">
+      <div className="mx-auto max-w-screen-lg">
+        <p className="text-center text-2xl">Not Found!</p>
+      </div>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
