11072c16e7df7018866fce74f899533edc28c4f1
sammy-code98 <sammysmart98@yahoo.com>
2022-01-04 16:21:50 +0100

    feat: update dashboard - add user auth listener -  add user hook - add userContext

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
+import useAuthListener from "./hooks/use-auth-listener";
+import UserContext from './context/user'
 const Login = lazy(() => import("./pages/Login"));
 const Signup = lazy(() => import("./pages/Signup"));
-const Dashboard = lazy(() => import("./pages/Dashboard"))
+const Dashboard = lazy(() => import("./pages/Dashboard"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
-
 function App() {
+  const { user } = useAuthListener();
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-          <Route path={ROUTES.SIGN_UP} element={<Signup />} />
-          <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-          <Route path="*" element={<NotFound />} />
-
-        </Routes>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Routes>
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+            <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
