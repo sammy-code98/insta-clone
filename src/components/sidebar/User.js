e86553d63b0cd4a2ba7ea188b3307efe15efa021
sammy-code98 <sammysmart98@yahoo.com>
2022-01-16 13:53:38 +0100

    feat: add user details - add proptypes and skeleton dependecies - add user card - refactor code

--- src/components/sidebar/User.js
+++ src/components/sidebar/User.js
@@ -1,10 +1,35 @@
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Skeleton from 'react-loading-skeleton';
+import { DEFAULT_IMAGE_PATH } from '../../constants/paths';
 
-function User() {
-    return (
-        <div>
-          user  
-        </div>
-    )
+export default function User({ username, fullName }) {
+  return !username || !fullName ? (
+    <Skeleton count={3} height={61} />
+  ) : (
+    <Link
+      to={`/p/${username}`}
+      className="grid grid-cols-4 gap-4  mb-6 items-center"
+    >
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full w-16  flex mr-3"
+          src={`/images/avatars/${username}.jpg`}
+          alt=""
+          onError={(e) => {
+            e.target.src = DEFAULT_IMAGE_PATH;
+          }}
+        />
+      </div>
+      <div className="col-span-3">
+        <p className="text-sm font-bold">{username}</p>
+        <p className="text-sm ">{fullName}</p>
+      </div>
+    </Link>
+  );
 }
 
-export default User
+User.propTypes = {
+  username: propTypes.string,
+  fullName: propTypes.string,
+};
