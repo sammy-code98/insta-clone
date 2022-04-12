6f5b60be550c07b71511a7cad7eb7a778ae76d72
sammy-code98 <sammysmart98@yahoo.com>
2022-04-12 05:20:16 +0100

    updating timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -7,14 +7,17 @@ function Timeline() {
     <div className="container col-span-2">
       {!photos ? (
         <>
-          {[...new Array(4)].map((_, index) => (
-            <Skeleton key={index} count={1} width={320} height={400} />
-          ))}
+            <Skeleton
+              count={1}
+              width={650}
+              height={600}
+              className="mb-5"
+            />
         </>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
       ) : (
-        photos?.length > 0 ? (
-          photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
-        ):(<p className="text-center text-2xl">Follow people to see photos</p>)
+        <p className="text-center text-2xl">Follow people to see photos</p>
       )}
     </div>
   );
---
+++ src/components/post/index.js
@@ -0,0 +1,6 @@
+import { useRef } from "react";
+import PropTypes from "prop-types";
+
+export default function Post() {
+  return <p>i am a post</p>;
+}
