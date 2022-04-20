9c91409038d70750509f7a87703e31b49712e8f6
sammy-code98 <sammysmart98@yahoo.com>
2022-04-20 12:49:53 +0100

    add like feature on image

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,11 +2,20 @@ import { useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
 import Image from "./image";
+import Actions from "./actions";
 export default function Post({ content }) {
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
   return (
-    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">   
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-8">
       <Header username={content.username} />
-      <Image src={content.imageSrc} caption={content.caption}/>
+      <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
     </div>
   );
 }
