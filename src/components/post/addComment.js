57011cc7cfe9d53150fda74c459e78d87df7706b
sammy-code98 <sammysmart98@yahoo.com>
2022-04-21 18:10:13 +0100

    comment submission

--- src/components/post/addComment.js
+++ src/components/post/addComment.js
@@ -2,7 +2,6 @@ import { useState, useContext } from "react";
 import PropTypes from "prop-types";
 import FirebaseContext from "../../context/firebase";
 import UserContext from "../../context/user";
-import { comment } from "postcss";
 
 export default function AddComment({
   docId,
@@ -18,11 +17,21 @@ export default function AddComment({
 
   const handleSUbmitComment = (e) => {
     e.preventDefault();
-    return null;
+
+    // display comments on ui
+    setComments([{displayName, comment},...comments]);
+    // create a new array, put comments in there  and then add the old comments
+    // clear input 
+    setComment('')
+
+    // update  and store firestore comment fields
+    return  firebase.firestore().collection('photos').doc(docId).update({
+      comments:FieldValue.arrayUnion({displayName,comment}),
+    });
   };
   return (
     <div className="border-t border-gray-primary">
-      <forn
+      <form
         className="flex justify-between pl-0 pr-5"
         method="POST"
         onSubmit={(event) =>
@@ -39,9 +48,18 @@ export default function AddComment({
           name="add-comment"
           placeholder="Add a comment..."
           value={comment}
-          onChange={({target})=> setComment(target.value)}
+          onChange={({ target }) => setComment(target.value)}
+          ref={commentInput}
         />
-      </forn>
+        <button
+          className={`text-sm font-bold text-blue-medium ${
+            !comment && "opacity-25"
+          }`}
+          type="submit"
+          disabled={comment.length < 1}
+          onClick={handleSUbmitComment}
+        >Post</button>
+      </form>
     </div>
   );
 }
