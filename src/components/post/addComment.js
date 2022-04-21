82b3a43b86ae416838480593caaf53bd6ee07e7c
sammy-code98 <sammysmart98@yahoo.com>
2022-04-21 17:47:05 +0100

    feat:add comment input field

--- src/components/post/addComment.js
+++ src/components/post/addComment.js
@@ -2,6 +2,7 @@ import { useState, useContext } from "react";
 import PropTypes from "prop-types";
 import FirebaseContext from "../../context/firebase";
 import UserContext from "../../context/user";
+import { comment } from "postcss";
 
 export default function AddComment({
   docId,
@@ -9,15 +10,45 @@ export default function AddComment({
   setComments,
   commentInput,
 }) {
-  const [coment, setComment] = useState("");
+  const [comment, setComment] = useState("");
   const { firebase, FieldValue } = useContext(FirebaseContext);
   const {
     user: { displayName },
   } = useContext(UserContext);
 
   const handleSUbmitComment = (e) => {
-      e.preventDefault();
-      return null;
-  }
-  return null;
+    e.preventDefault();
+    return null;
+  };
+  return (
+    <div className="border-t border-gray-primary">
+      <forn
+        className="flex justify-between pl-0 pr-5"
+        method="POST"
+        onSubmit={(event) =>
+          comment.length >= 1
+            ? handleSUbmitComment(event)
+            : event.preventDefault()
+        }
+      >
+        <input
+          arial-label="Add a comment"
+          autoComplete="off"
+          className="text-sm text-gray-base w-full mr-3 py-5 px-4"
+          type="text"
+          name="add-comment"
+          placeholder="Add a comment..."
+          value={comment}
+          onChange={({target})=> setComment(target.value)}
+        />
+      </forn>
+    </div>
+  );
 }
+
+AddComment.propTypes = {
+  docId: PropTypes.string.isRequired,
+  comments: PropTypes.array.isRequired,
+  setComments: PropTypes.func.isRequired,
+  commentInput: PropTypes.object,
+};
