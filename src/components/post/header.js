1350aa59bac78029bc1d256e4999b3f5dfab0425
sammy-code98 <sammysmart98@yahoo.com>
2022-04-26 16:23:07 +0100

    fix:add defualt images to all image path

--- src/components/post/header.js
+++ src/components/post/header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { DEFAULT_IMAGE_PATH } from "../../constants/paths"
+
 export default function Header({ username }) {
   return (
     <div className="flex border-b  border-gray-primary h-4 p-4 py-8">
@@ -10,6 +12,9 @@ export default function Header({ username }) {
             className="rounded-full h-8 w-8 flex mr-3"
             src={`/images/avatars/${username}.jpg`}
             alt={`${username} profile pic`}
+            onError={(e) => {
+              e.target.src = DEFAULT_IMAGE_PATH;
+            }}
           />
         </Link>
       </div>
--- src/components/post/image.js
+++ src/components/post/image.js
@@ -1,6 +1,16 @@
 import PropTypes from "prop-types";
+import { DEFAULT_POST_IMAGE } from "../../constants/paths";
+
 export default function Image({ src, caption }) {
-  return <img src={src} alt={caption} />;
+  return (
+    <img
+      src={src}
+      alt={caption}
+      onError={(e) => {
+        e.target.src = DEFAULT_POST_IMAGE;
+      }}
+    />
+  );
 }
 
 Image.propTypes = {
