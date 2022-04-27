498926eeb246fec881f77eb757f6fcfe253b6806
sammy-code98 <sammysmart98@yahoo.com>
2022-04-27 10:47:26 +0100

    feat: display image on user profile

--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -1,10 +1,34 @@
 import PropTypes from "prop-types";
-import Skeleton from 'react-loading-skeleton'
-export default function Photos({photos}){
-    return null
+import Skeleton from "react-loading-skeleton";
+import { DEFAULT_PROFILE_IMAGE } from "../../constants/paths";
 
+export default function Photos({ photos }) {
+  console.log("photos", photos);
+  return (
+    <div className="h-16 border-t border-gray-primary mt-12 pt-4">
+      <div className="grid grid-cols-3 gap-8 mt-4 mb-12">
+        {!photos ? (
+          <>
+            <Skeleton count={12} width={320} height={400} />
+          </>
+        ) : photos.length > 0 ? (
+          photos.map((photo) => (
+            <div key={photo.docId} className="relative group">
+              <img
+                src={photo.imageSrc}
+                alt={photo.caption}
+                onError={(e) => {
+                  e.target.src = DEFAULT_PROFILE_IMAGE;
+                }}
+              />
+            </div>
+          ))
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 Photos.propTypes = {
-    photos: PropTypes.array.isRequired
-}
+  photos: PropTypes.array.isRequired,
+};
